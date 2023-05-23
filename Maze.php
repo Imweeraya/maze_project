9c93e56b8ac3c4c9009da70b85e345217940495a
ohm2303 <95691732+ohm2303@users.noreply.github.com>
2023-05-23 22:03:38 +0700

    ..

--- Maze.php
+++ Maze.php
@@ -59,7 +59,7 @@
             <button class="Restart-button" onclick="restart()"> Restart </button>
         </div>
 
-    
+        <?php // Local?>
 </body>
 
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
